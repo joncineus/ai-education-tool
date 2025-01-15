484e16afa28ba13dcb85a807834a4870b404e4dc
Ayush Pratap Singh <ayushpratap3154@gmail.com>
2025-01-16 00:07:17 +0530

    Update Logo.jsx

--- frontend/src/Logo.jsx
+++ frontend/src/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <img src="../FikraSpace_Logo_Transparent.png" alt="FikraSpace Logo" 
+    <img src="/FikraSpace_Logo_Transparent.png" alt="FikraSpace Logo" 
     className='max-w-[10rem]'/> //Logo that will be displayed on the top left corner of the page
   )
 }
